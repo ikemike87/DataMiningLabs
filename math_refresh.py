95320009efe3fb8926e05916417e7d1fa2aeb4d0
Mike <michael.bamford@mymail.champlain.edu>
2021-09-10 16:57:49 -0400

    complete math refresh and start pandas

--- math_refresh.py
+++ math_refresh.py
@@ -9,6 +9,7 @@ Original file is located at
 
 import numpy as np
 from scipy import stats
+from sklearn.linear_model import LinearRegression
 import matplotlib
 
 randnums = np.random.rand(100)
@@ -24,7 +25,13 @@ spearmanr = stats.spearmanr(randnums, randnums)
 spearmanr2 = stats.spearmanr(randnums, randnums2)
 log = np.log(randnums)
 exp = randnums**2
+exp2 = 2**randnums
 summ = sum(randnums)
+z = randnums + randnums2
+x = randnums.reshape(-1,1)
+z = z.reshape(-1,1)
+regression = LinearRegression().fit(x,z)
+regScore = regression.score(x,z)
 
 print("100 random numbers = " + str(randnums) + "\n")
 print("mean = " + str(mean) + "\n")
@@ -38,7 +45,9 @@ print("Rank Correlation 1 = " + str(spearmanr) + "\n")
 print("rank Correlation 2 = " + str(spearmanr2) + "\n")
 print("Log = " + str(log) + "\n")
 print("exp = " + str(exp) + "\n")
+print("exp2 = " + str(exp2) + "\n")
 print("Summ = " + str(summ) + "\n")
+print("Regression = " + str(regScore) + "\n")
 
 print("scatter plot")
 matplotlib.pyplot.scatter(randnums, randnums2)
